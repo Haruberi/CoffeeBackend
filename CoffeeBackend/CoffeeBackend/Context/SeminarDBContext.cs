0758ceb69ddd77fc761214e4655604e304cc679d
Anna <anna.h.hallberg.92@gmail.com>
2020-10-13 18:58:22 +0200

    Add Connectionstring

--- CoffeeBackend/CoffeeBackend/Context/SeminarDBContext.cs
+++ CoffeeBackend/CoffeeBackend/Context/SeminarDBContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoffeeBackend.Context
 {
-    public class SeminarDBContext
+    public class SeminarDBContext : DbContext
     {
+        //List entities
+        public DbSet<Seminar> seminars { get; set; }
+        public DbSet<Student> students { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
     }
 }
